1cb60ee0ba671dcb80a7720d7eb6fc8c728757df
mahrokh sadro <msadrolodabaee@myseneca.ca>
2022-01-19 22:59:39 -0500

    fixx

--- src/pages/UpdatePage.js
+++ src/pages/UpdatePage.js
@@ -4,64 +4,35 @@ const UpdatePage = () => {
   const [formData, setFormData] = useState({
     id: "",
     name: "",
-    price: 0,
-    // description: "",
-    // category: "",
-    // quantity: 0,
-    // photoURL: "",
-    // sold: 0,
-    // shipping: false,
   });
 
   const [product, setProduct] = useState({
     id: "",
     name: "",
-    // price: 0,
-    // description: "",
-    // category: "",
-    // quantity: 0,
-    // photoURL: "",
-    // sold: 0,
-    // shipping: false,
+    price: 0,
+    description: "",
+    category: "",
+    quantity: 0,
+    photoURL: "",
+    sold: 0,
+    shipping: false,
   });
-  const showProductInfo = (evt) => {
-    evt.preventDefault();
-
-    fetch(
-      `https://shopify-inventory-backend.herokuapp.com/products/${product.id}`
-    )
-      .then((response) => response.json())
-      .then((json) => {
-        // console.log("id:" + product.id);
-        setProduct(json.data);
-        // setFormData(json.data);
-        console.log(product);
-        // console.log(formData);
-        // alert(json.message);
-      })
-      .catch((err) => {
-        console.log(`Error ${err}`);
-      });
-
-    fetch(
-      `https://shopify-inventory-backend.herokuapp.com/products/${product.id}`
-    )
-      .then((response) => response.json())
-      .then((json) => {
-        // console.log("id:" + product.id);
-        // setProduct(json.data);
-        setFormData(json.data);
-        // console.log(product);
-        console.log(formData);
-        // alert(json.message);
-      })
-      .catch((err) => {
-        console.log(`Error ${err}`);
-      });
-  };
 
   const updateProduct = (evt) => {
     evt.preventDefault();
+
+    fetch(
+      `https://shopify-inventory-backend.herokuapp.com/products/${formData.id}`
+    )
+      .then((response) => response.json())
+      .then((json) => {
+        setProduct(json.data);
+
+        console.log("---" + product);
+      })
+      .catch((err) => {
+        console.log(`Error ${err}`);
+      });
 
     fetch(
       `https://shopify-inventory-backend.herokuapp.com/products/${formData.id}`,
@@ -70,22 +41,14 @@ const UpdatePage = () => {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(formData),
+        body: JSON.stringify({ ...product, quantity: formData.quantity }),
       }
     )
       .then((response) => response.json())
       .then((json) => {
         alert(json.message);
         setFormData({
-          id: "",
           name: "",
-          price: 0,
-          description: "",
-          category: "",
-          quantity: 0,
-          photoURL: "",
-          sold: 0,
-          shipping: false,
         });
       })
       .catch((err) => {
@@ -99,7 +62,7 @@ const UpdatePage = () => {
         <div className="container">
           <h1>Update</h1>
 
-          <form action="" onSubmit={showProductInfo}>
+          <form action="" onSubmit={updateProduct}>
             <div className="form-control">
               <label htmlFor="id">
                 Enter the product ID you want to Update
@@ -107,141 +70,32 @@ const UpdatePage = () => {
               <input
                 type="text"
                 id="id"
-                value={product.id}
+                value={formData.id}
                 onChange={(evt) => {
-                  setProduct({ ...product, id: evt.target.value });
+                  setFormData({ ...formData, id: evt.target.value });
                 }}
               />
             </div>
 
             <div className="form-control">
+              <label htmlFor="name">new name</label>
+              <input
+                type="text"
+                id="name"
+                value={formData.name}
+                onChange={(evt) => {
+                  setProduct({ ...formData, name: evt.target.value });
+                }}
+              />
+            </div>
+            <div className="form-control">
               <button className="btn" type="submit">
-                Show Info
+                Update the Quantity
               </button>
             </div>
           </form>
         </div>
       </section>
-      {/* ///////////////////////////// ///////////////////////////////////////////////////////////*/}
-      <form action="" onSubmit={updateProduct}>
-        <div className="form-control">
-          <label htmlFor="name">name</label>
-          <input
-            type="text"
-            id="name"
-            value={product.name}
-            onChange={(evt) => {
-              setFormData({ name: evt.target.value });
-            }}
-          />
-        </div>
-        {/* <div className="form-control">
-          <label htmlFor="price">price</label>
-          <input
-            type="number"
-            id="price"
-            value={product.price}
-            onChange={(evt) => {
-              setFormData({ price: evt.target.value });
-            }}
-          />
-        </div> */}
-        <div className="form-control">
-          <button className="btn" type="submit">
-            Update Item
-          </button>
-        </div>
-      </form>
-
-      {/*   <div className="form-control">
-              <label htmlFor="description">description</label>
-              <input
-                type="text"
-                id="description"
-                value={formData.description}
-                onChange={(evt) => {
-                  setFormData({ ...formData, description: evt.target.value });
-                }}
-              />
-            </div>
-
-            <div className="form-control">
-              <label htmlFor="category">category</label>
-              <input
-                type="text"
-                id="category"
-                valeu={formData.category}
-                onChange={(evt) => {
-                  setFormData({ ...formData, category: evt.target.value });
-                }}
-              />
-            </div>
-
-            <div className="form-control">
-              <label htmlFor="quantity">quantity</label>
-              <input
-                type="number"
-                id="quantity"
-                value={formData.quantity}
-                onChange={(evt) => {
-                  setFormData({
-                    ...formData,
-                    quantity: evt.target.value,
-                  });
-                }}
-              />
-            </div>
-
-            <div className="form-control">
-              <label htmlFor="photoURL">photoURL</label>
-              <input
-                type="text"
-                id="photoURL"
-                value={formData.photoURL}
-                onChange={(evt) => {
-                  setFormData({
-                    ...formData,
-                    photoURL: evt.target.value,
-                  });
-                }}
-              />
-            </div>
-            <div className="form-control">
-              <label htmlFor="sold">sold</label>
-              <input
-                type="number"
-                id="sold"
-                value={formData.sold}
-                onChange={(evt) => {
-                  setFormData({
-                    ...formData,
-                    sold: evt.target.value,
-                  });
-                }}
-              />
-            </div>
-            <div className="form-control">
-              <label htmlFor="shipping">shipping</label>
-              <input
-                type="text"
-                id="shipping"
-                value={formData.shipping}
-                onChange={(evt) => {
-                  setFormData({
-                    ...formData,
-                    shipping: evt.target.value,
-                  });
-                }}
-              />
-            </div> */}
-      {/* <div className="form-control">
-              <button className="btn" type="submit">
-                Update Item
-              </button>
-            </div> */}
-      {/* </form>
-        </div>
-      </section> */}
     </div>
   );
 };
